cca1552d4a5b5af2b6b3c9375bd41a499ec1eb2a
FrancisLawlor <franlawlor0@gmail.com>
2017-08-03 21:08:15 +0100

    Changed display image filename. Refactored code.

--- clipboard.py
+++ clipboard.py
@@ -7,25 +7,22 @@ from Tkinter import *
 # Get value inserted to entry at time of rendering.
 def getValue(entry):
     return lambda: copyToClipboard(entry)
-    
-# Copy the contents of the entry field to the clipboard by clearing and then appending.
+
 def copyToClipboard(entry):
     entry.clipboard_clear()
     entry.clipboard_append(entry.get())
 
-# Insert lines from file to entry fields in the gui.
 def drawGui(filename):
     f = open(filename,'r')
 
     window = Tkinter.Tk()
     window.title="Clipboard"
 
-    # Row number
     r = 1
 
     for line in f:
         entry = Entry()
-        
+
         # Add each line from the file without the '\n' character.
         entry.insert(END, line[:-1])
         entry.grid(row = r, column = 2)
@@ -37,8 +34,6 @@ def drawGui(filename):
 
 filename = ""
 
-# Handle filename input.
-
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'l:')
 except getopt.GetoptError:
@@ -48,15 +43,15 @@ except getopt.GetoptError:
 for opt, arg in opts:
     if opt in ('-l'):
         filename = arg
-        if(filename == ""):
+        if (filename == ""):
             print("Filename required.")
             sys.exit(2)
     else:
         print("Invalid Input")
         sys.exit(2)
 
-if(filename == ""):
+if (filename == ""):
     print("Filename required.")
     sys.exit(2)
-            
+
 drawGui(filename)
